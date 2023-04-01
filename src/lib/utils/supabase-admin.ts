8f7e9c4ea1cc2bd97bbc9843a1904c76b5a9b865
Toku <61105719+Bird109@users.noreply.github.com>
2023-04-01 13:47:41 +0900

    Update supabase-admin.ts

--- src/lib/utils/supabase-admin.ts
+++ src/lib/utils/supabase-admin.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = 'https://scbduockldhfkdbpauhr.supabase.co';
+const url = 'https://aelgrzrwfmtmzbspsosn.supabase.co';
 const secret =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjYmR1b2NrbGRoZmtkYnBhdWhyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0OTUwNzQ5MCwiZXhwIjoxOTY1MDgzNDkwfQ.uo0XuFNg3v-3mliBCvo8eHSdWHN-IYPlLJWaW1J0lm4';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbGdyenJ3Zm10bXpic3Bzb3NuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MDI1MjM3OCwiZXhwIjoxOTk1ODI4Mzc4fQ.YD7jbVAqwcfCh4q1bv-H1_Wuk01x8rbcm71S-B54KfM';
 
 export const admin = createClient(url, secret);
