d435eb1cc96b744aab74284787668e1ccb47a4f1
Toku <61105719+Bird109@users.noreply.github.com>
2023-04-04 22:38:53 +0900

    Update auth.ts

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -2,4 +2,4 @@ export const validateEmail = (email: string): boolean => {
 	return email.includes('@');
 };
 
-export const start_time = Date.parse('2023/04/04 13:39:00 UTC');
+export const start_time = Date.parse('2023/04/04 13:45:00 UTC');
