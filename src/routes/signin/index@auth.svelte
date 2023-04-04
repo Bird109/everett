e9fd7ba9e4500c44bece66d1070576bb2e623354
Toku <61105719+Bird109@users.noreply.github.com>
2023-04-04 14:10:52 +0900

    Update index@auth.svelte

--- src/routes/signin/index@auth.svelte
+++ src/routes/signin/index@auth.svelte
@@ -36,7 +36,7 @@
 		{#if message}
 			<p class="text-sm text-red-400">{message}</p>
 		{/if}
-		<ButtonPrimary on:click={signin} disabled={loading}>Continue</ButtonPrimary>
+		<ButtonPrimary on:click={signin} disabled={loading}>Continue</tickets>
 		<div class="mx-auto w-fit mt-1">
 			<a class="text-sm text-primary hover:underline cursor-pointer" href="/register">新規登録</a>
 		</div>
